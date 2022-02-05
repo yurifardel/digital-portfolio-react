42e468b1bb635cd5f9a97291e5cb6eeb64ddd9f8
yuri fardel <yrnascimento@outlook.com>
2022-02-05 16:44:39 -0300

    feat: create login page

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Style from './login-style.scss'
 
 const Login: React.FC = () => {
   return (
-    <h1>hello world</h1>
+    <div className={Style.login}>
+      <header className={Style.header}>
+        <h1>Portifolio</h1>
+      </header>
+      <form className={Style.form}>
+        <div className={Style.input}>
+          <input type="text" />
+        </div>
+        <button type='submit'>Entrar</button>
+        <a className={Style.link} href="">Cadastre-se</a>
+      </form>
+      <footer></footer>
+    </div>
   )
 }
 
